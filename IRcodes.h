48abecafc28c7835bbfaa6f8fea0ced0dbbae91d
Daniele Monteleone <daniele.monteleone.it@gmail.com>
2015-07-16 13:50:44 +0200

    added some codes

--- IRcodes.h
+++ IRcodes.h
@@ -1,12 +1,42 @@
-#ifndef POWER
-#define POWER 0xF4BA2988
+#ifndef BTNPOWER
+#define BTNPOWER 0xF4BA2988
 #endif
 #ifndef RED
-#define RED 0xDAEA83EC
+#define BTNRED 0xDAEA83EC
 #endif
-#ifndef GREEN
-#define GREEN 0x2BAFCEEC
+#ifndef BTNGREEN
+#define BTNGREEN 0x2BAFCEEC
 #endif
-#ifndef BLUE
-#define BLUE 0x71A1FE88
+#ifndef BTNBLUE
+#define BTNBLUE 0x71A1FE88
+#endif
+#ifndef BTN1
+#define BTN1 0xE13DDA28
+#endif
+#ifndef BTN2
+#define BTN2 0xAD586662
+#endif
+#ifndef BTN3
+#define BTN3 0x273009C4
+#endif
+#ifndef BTN4
+#define BTN4 0xF5999288
+#endif
+#ifndef BTN5
+#define BTN5 0x731A3E02
+#endif
+#ifndef BTN6
+#define BTN6 0x2C452C6C
+#endif
+#ifndef BTN7
+#define BTN7 0x4592E14C
+#endif
+#ifndef BTN8
+#define BTN8 0x6825E53E
+#endif
+#ifndef BTN9
+#define BTN9 0x8B8510E8
+#endif
+#ifndef BTN0
+#define BTN0 0xB9F56762
 #endif
