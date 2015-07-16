039bac98a9eb6f562ae6405536508c65406dc2f1
Daniele Monteleone <daniele.monteleone.it@gmail.com>
2015-07-16 12:46:32 +0200

    added header file for IR codes

---
+++ IRcodes.h
@@ -0,0 +1,12 @@
+#ifndef POWER
+#define POWER 0xF4BA2988
+#endif
+#ifndef RED
+#define RED 0xDAEA83EC
+#endif
+#ifndef GREEN
+#define GREEN 0x2BAFCEEC
+#endif
+#ifndef BLUE
+#define BLUE 0x71A1FE88
+#endif
